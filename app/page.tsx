5924f9464e9133b20b660c11f59954ea08f06e9b
rain <15911122312@163.com>
2024-11-05 16:07:20 +0800

    feat: add coco-website

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,116 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [isChat, setIsChat] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+  const chatOrSearch = () => {
+    setIsChat((prev) => !prev);
+  };
+
+  return (
+    <main className="min-h-screen bg-black">
+      <div className="absolute inset-0 pointer-events-none"></div>
+
+      <div className="relative z-10 container mx-auto px-4 py-16 bg-[url('/images/bg.png')] bg-cover bg-center">
+        <div className="flex justify-center items-center mb-8">
+          <Image
+            className="dark:invert"
+            src="/images/logo.png"
+            alt="Coco logo"
+            width={58}
+            height={42}
+            priority
+          />
+        </div>
+
+        <div className="text-center max-w-3xl mx-auto mt-14 mb-12">
+          <Image
+            className="dark:invert"
+            src="/images/slogan.png"
+            alt="Coco slogan"
+            width={836}
+            height={184}
+            priority
+          />
+          <p className="text-lg text-gray-400 mt-6 mb-8">
+            Coco AI unifies all your enterprise applications and data—Google
+            Workspace, Dropbox, GitHub, and more—into one powerful search and
+            Gen-AI chat platform.
+            <br />
+            Instantly access and interact with your team’s unique knowledge,
+            driving collaboration and insights across your data.
+          </p>
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://github.com/infinilabs/coco-app"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              className="dark:invert mx-auto"
+              src="/images/Github.png"
+              alt="Coco Github"
+              width={148}
+              height={50}
+              priority
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <div className="max-w-2xl mx-auto backdrop-blur-xl rounded-2xl">
+          <div className="relative mb-6">
+            {isChat ? (
+              <Image
+                className="dark:invert"
+                src="/images/chat.png"
+                alt="Coco chat"
+                width={776}
+                height={513}
+                priority
+              />
+            ) : (
+              <Image
+                className="dark:invert"
+                src="/images/Search.png"
+                alt="Coco Search"
+                width={776}
+                height={513}
+                priority
+              />
+            )}
+            <div
+              className="absolute top-16 right-7 cursor-pointer"
+              onClick={chatOrSearch}
+            >
+              {isChat ? (
+                <Image
+                  className="dark:invert"
+                  src="/images/chat-button.png"
+                  alt="Coco chat-button"
+                  width={68}
+                  height={22}
+                  priority
+                />
+              ) : (
+                <Image
+                  className="dark:invert"
+                  src="/images/search-button.png"
+                  alt="Coco search-button"
+                  width={68}
+                  height={22}
+                  priority
+                />
+              )}
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-28 flex justify-center text-white text-opacity-60">
+          ©2024 INFINI Labs, All Rights Reserved.
+        </div>
+      </div>
+    </main>
   );
 }
