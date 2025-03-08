f9dc4989639f6a6587b01888abd42189fd1ad7c1
hardy <luohf@infinilabs.com>
2025-03-08 10:07:10 +0800

    chore: just use the out folder

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+const isDev = process.env.NODE_ENV === "development";
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
@@ -8,7 +10,7 @@ const nextConfig: NextConfig = {
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     unoptimized: true,
   },
-  distDir: "docs",
+  distDir: isDev ? "out" : "docs",
 };
 
 export default nextConfig;
