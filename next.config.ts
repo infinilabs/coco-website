d9e010b74badf74fa2218565e488e64db469cf91
Medcl <m@medcl.net>
2025-05-31 23:15:23 +0800

    chore: fix cors (#31)

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,14 @@ const nextConfig: NextConfig = {
   images: {
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+contentSecurityPolicy: `
+  default-src 'self';
+  script-src 'self' https://coco.infini.cloud;
+  style-src 'self' 'unsafe-inline' https://coco.infini.cloud;
+  img-src 'self' data: https://coco.infini.cloud;
+  connect-src 'self' https://coco.infini.cloud;
+  frame-src https://coco.infini.cloud;
+`,
     unoptimized: true,
   },
   distDir: isDev ? "out" : "docs",
