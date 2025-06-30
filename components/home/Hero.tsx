232ea9b6b46df5e3f687b13197d5c39e12bf53a4
rain9 <15911122312@163.com>
2025-06-30 17:52:44 +0800

    fix: privacy router

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import { useTheme } from "next-themes";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
 import Searchbox from "@/components/Searchbox";
 
 const Hero = ({ locale, langName }: { locale: any; langName: string }) => {
-  const { theme } = useTheme();
-
   const [mounted, setMounted] = useState(false);
   useEffect(() => setMounted(true), []);
   if (!mounted) return null;
