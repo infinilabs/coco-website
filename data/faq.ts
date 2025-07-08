c01f16e33b5ddeaa3e7ced04f7236f876dae4fb8
rain9 <15911122312@163.com>
2025-07-08 17:44:55 +0800

    style: remove border

--- data/faq.ts
+++ data/faq.ts
@@ -38,32 +38,32 @@ export const FAQ_EN = [
 
 export const FAQ_ZH = [
   {
-    question: "Coco AI 是完全开源的吗?",
+    question: "Coco AI 是完全开源的吗？",
     answer:
       "是的，Coco AI 的核心组件均为开源项目。Coco App 采用 MIT 协议，Coco Server 采用 AGPL-3.0 协议。\n 在开源协议的指导下，您可自由下载安装使用、修改和商用，无需额外获得我们的许可。",
   },
   {
-    question: "Coco AI 的搜索功能如何运作?",
+    question: "Coco AI 的搜索功能如何运作？",
     answer:
       "Coco AI 提供统一搜索入口，支持本地、云端、多模态搜索，可同时检索文件、应用和各类云端数据源（如 Google Drive、Notion、Yuque、Hugo、RSS 等），并支持文字、语音输入。",
   },
   {
-    question: "Coco AI 如何保证我的数据安全?",
+    question: "Coco AI 如何保证我的数据安全？",
     answer:
       "Coco AI 支持完全离线部署，内部数据内部处理；您也可以选择私有云部署，保障数据安全与合规要求。",
   },
   {
-    question: "Coco AI 的本地部署方式?",
+    question: "Coco AI 的本地部署方式？",
     answer:
       "本地部署主要针对 Coco Server，用于扩展企业级功能，运行后可访问后台管理页面，配置数据源、模型和助手等，部署方式灵活，支持 macOS、Linux、Windows，也可通过 Docker 快速搭建。<a href='https://docs.infinilabs.com/coco-server/main/docs/getting-started/install/' target='_blank' rel='noopener noreferrer' style='color: #14C4C9; text-decoration: underline;'>[查看部署指南]</a>",
   },
   {
-    question: "不部署 Coco Server，我可以单独使用 Coco App 吗?",
+    question: "不部署 Coco Server，我可以单独使用 Coco App 吗？",
     answer:
       "可以。您可以单独安装 Coco App，无需部署 Coco Server，并且依然可以使用本地文件搜索和应用快捷启动，以及丰富的插件生态。\n 此外，您还可以选择连接 Coco Cloud 提供的在线服务，体验云端的搜索、AI 能力，进一步扩展应用场景。",
   },
   {
-    question: "Coco App 可以连接多个 Coco Server 吗?",
+    question: "Coco App 可以连接多个 Coco Server 吗？",
     answer:
       "可以。Coco App 支持同时连接多个 Coco Server，您可以在 App 中配置不同的服务器地址，支持多个 Coco Server 混合搜索，可以切换和管理多个部署环境。这对于需要跨团队、跨部门使用不同知识库和 AI 助手的用户非常有帮助。",
   },
